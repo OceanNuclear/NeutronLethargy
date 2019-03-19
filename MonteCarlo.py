f426867bf7bdac086dad1b66ab1cf28e4a41ee17
OceanNuclear <annoyingamygdala@gmail.com>
2019-03-19 15:59:50 +0000

    Updated code comments

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,25 +1,28 @@
 #!/home/ocean/anaconda3/bin/python3
+#Written by Ocean Wong, last updated 2019-03-19 15:57:53, 
+#For calculating the number of collisions required to slow down a neutron to thermal speed using Monte Carlo method.
+
 from numpy import cos, arccos, sin, arctan, tan, pi, sqrt; from numpy import array as ary; import numpy as np; tau = 2*pi
 from matplotlib import pyplot as plt
 from numpy.random import uniform as uni
 
-NumSim=300000
-A=2
+NumSim=300000 #Decide the number of trials to run.
+A=2	#Atomic mass
 alpha = ((A-1)/(A+1))**2
 MeV=1E6
 eV=1
 
-def Thermalize():
+def Thermalize(): #function to calculate number of collisions required to thermalize.
 	NumCol=0
-	E_n = 2*MeV
-	ThermalEnergy = 1*eV
+	E_n = 2*MeV#Starting energy
+	ThermalEnergy = 1*eV#thermal threshold, below which we "kill" the particle
 	while (E_n > ThermalEnergy):
 		NumCol+=1
 		E_loss_factor=uni(alpha,1)
 		E_n=E_n*E_loss_factor
 	return NumCol
 
-ColToTherm=[]
+ColToTherm=[]#Store the results as a 1D array so that a histogram can be plotted later.
 for n in range(NumSim):
 	NumCol = Thermalize()
 	ColToTherm.append(NumCol)
