6406e452e73d168b74702b1ebb45af66ec765460
OceanNuclear <annoyingamygdala@gmail.com>
2019-03-19 13:53:20 +0000

    Created python script to plot Figures

--- IllustrationPlotters.py
+++ IllustrationPlotters.py
@@ -11,7 +11,7 @@ x_rect=np.concatenate([[0,alpha],x,[1]])
 y=alpha*(x-x+1)#hack to make y=alpha with the same shape of x, quickly.
 y_rect=np.concatenate([[0,0],y,[0]])
 
-Figure = "Fig2"
+Figure = "Fig1"
 
 if Figure=="Fig1":
 	#Create other distributions
@@ -25,34 +25,39 @@ if Figure=="Fig1":
 	ax.set_xticks([alpha,1])
 	ax.set_xticklabels([r"$\alpha E_i$",r"$E_i$"])
 	ax.set_xlabel(r"Final energy after collision $E_f$")
+	ax.set_xlim(0,1.004)
 
 if Figure=="Fig2":
 	x2 = np.linspace(0,-np.log(alpha))
 	y2 = x2
-	ax.plot(x2,y2,label="$ \Delta u$")
 	#make prob dist look nicer
 	x2rect=np.concatenate([[0],x2,[-np.log(alpha),1]])
 	y2rect=np.concatenate([[0],0.4*np.e**(-x2),[0,0]])
 	ax.plot(x2rect,y2rect,label=r"$P(\Delta u)=\frac{dE}{d(\Delta u)}P(E)$")
+	ax.plot(x2,y2,label="$ \Delta u$")
 	ax.set_xlabel(r"$\Delta u$")
-	ax.set_xticks([0,np.log(-alpha)])
+	ax.set_xticks([0,np.log(1/alpha)])
 	ax.set_xticklabels(["0",r"-ln($\alpha$)"])
-
+	ax.set_xlim(-0.008,1)
 
 if Figure=="Fig3":
-	xlong=np.linspace(0,1)
+	xlong=np.linspace(0.2,1,100)
 	ylong=np.log(1/xlong)
 	ax.plot(x_rect,y_rect,label="P(E)")
 	ax.plot(xlong,ylong,label=r"$\Delta$ u(E)")
+
+	midpt=(alpha+1)/2
 	if "Dotted line":
-		xdot=[]
-		ydot=[]
-		xdot.append(1)
-		ydot.append(1)
+		xdot=[midpt]
+		ydot=[0.4]
+		xdot.append(midpt)
+		ydot.append(np.log(1/midpt))
+		xdot.append(0)
+		ydot.append(np.log(1/midpt))
 	ax.plot(xdot,ydot, linestyle="--")
+	ax.set_xlim(0,1.004)
 
 #set y axes, legend, and save.
-ax.set_xlim(0,1)
 ax.set_ylim(bottom=0)
 ax.set_yticks([])
 ax.legend()
