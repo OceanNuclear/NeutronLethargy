98191cf1f7255d7d28ab0037ba0a8ef674065b45
OceanNuclear <annoyingamygdala@gmail.com>
2021-08-15 10:48:47 +0100

    Updated MonteCarlo.py with black

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,34 +1,44 @@
-#!/home/ocean/anaconda3/bin/python3
-#Written by Ocean Wong, last updated 2019-03-19 15:57:53, 
-#For calculating the number of collisions required to slow down a neutron to thermal speed using Monte Carlo method.
+"""Written by Ocean Wong, last updated 2019-03-19 15:57:53, 
+For calculating the number of collisions required to slow down a neutron to thermal speed using Monte Carlo method."""
 
-from numpy import cos, arccos, sin, arctan, tan, pi, sqrt; from numpy import array as ary; import numpy as np; tau = 2*pi
+from numpy import cos, arccos, sin, arctan, tan, pi, sqrt
+from numpy import array as ary
+import numpy as np
+
+tau = 2 * pi
 from matplotlib import pyplot as plt
 from numpy.random import uniform as uni
 
-NumSim=300000 #Decide the number of trials to run.
-A=2	#Atomic mass
-alpha = ((A-1)/(A+1))**2
-MeV=1E6
-eV=1
-StartingE=2*MeV
-ThermalEnergy = 1*eV#thermal threshold, below which we "kill" the particle
+NumSim = 300000  # Decide the number of trials to run.
+A = 2  # Atomic mass
+alpha = ((A - 1) / (A + 1)) ** 2
+MeV = 1e6
+eV = 1
+StartingE = 2 * MeV
+ThermalEnergy = 1 * eV  # thermal threshold, below which we "kill" the particle
 
-def Thermalize(): #function to calculate number of collisions required to thermalize.
-	NumCol=0
-	E_n = StartingE
-	while (E_n > ThermalEnergy):
-		NumCol+=1
-		E_loss_factor=uni(alpha,1)
-		E_n=E_n*E_loss_factor
-	return NumCol
 
-ColToTherm=[]#Store the results as a 1D array so that a histogram can be plotted later.
+def Thermalize():  # function to calculate number of collisions required to thermalize.
+    NumCol = 0
+    E_n = StartingE
+    while E_n > ThermalEnergy:
+        NumCol += 1
+        E_loss_factor = uni(alpha, 1)
+        E_n = E_n * E_loss_factor
+    return NumCol
+
+
+ColToTherm = (
+    []
+)  # Store the results as a 1D array so that a histogram can be plotted later.
 for n in range(NumSim):
-	NumCol = Thermalize()
-	ColToTherm.append(NumCol)
+    NumCol = Thermalize()
+    ColToTherm.append(NumCol)
 print(np.mean(ColToTherm))
-plt.hist(ColToTherm,bins=max(ColToTherm)-min(ColToTherm)+1)
+plt.hist(ColToTherm, bins=max(ColToTherm) - min(ColToTherm) + 1)
 plt.show()
 
-print("analytical result requies number of collisions = ",np.log(StartingE/ThermalEnergy)/(1+alpha/(1-alpha)*np.log(alpha)))
+print(
+    "analytical result requies number of collisions = ",
+    np.log(StartingE / ThermalEnergy) / (1 + alpha / (1 - alpha) * np.log(alpha)),
+)
